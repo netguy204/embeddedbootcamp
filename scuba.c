debec5f67c84e077b8a9c8c11f88fe0c0c0539c1
Hittherhod <srhody92@gmail.com>
2016-02-11 12:08:56 -0500

    Silencing compiler warnings

--- scuba.c
+++ scuba.c
@@ -12,6 +12,7 @@
 **/
 
 #include <stdint.h>
+#include <assert.h>
 
 #include "os.h"
 
@@ -103,6 +104,7 @@ adc2rate(int32_t adc) {
   } else {
     assert(0);
   }
-            
+  // NOTE: Silencing compiler warning
+  return 0;
 }
 

f31be649169a32795658c111aa0486feddf5e946
Brian Taylor <el.wubo@gmail.com>
2016-02-11 11:22:05 -0500

    lcd almost done

--- calculator_lcd.c
+++ calculator_lcd.c
@@ -21,15 +21,23 @@ static void lcd_printf(uint8_t line, const char* msg, ...) {
 }
 
 #define MM_TO_M(mm) (mm / 1000)
-
+#define MM_TO_FT(mm) (mm / 305) // 304.8
+#define ML_TO_L(ml) (ml / 1000)
 
 void calculator_lcd_update(struct CalculationState* state) {
   lcd_printf(0, "SCUBIE DUUBA");
   if(state->display_units == CALC_UNITS_METRIC) {
     lcd_printf(3, "DEPTH: %4u M", MM_TO_M(state->depth_mm));
+    lcd_printf(4, "RATE: %4u M", MM_TO_M(state->rate_mm_per_m));
+    
   } else {
-    // ft
+    lcd_printf(3, "DEPTH: %4u FT", MM_TO_FT(state->depth_mm));
+    lcd_printf(4, "RATE: %4u FT", MM_TO_FT(state->rate_mm_per_m));
   }
+  
+  lcd_printf(5, "AIR: %4u L", ML_TO_L(state->depth_mm));
+  lcd_printf(6, "EDT: ");
+  lcd_printf(8, "Alarm: ");
 }
 
 

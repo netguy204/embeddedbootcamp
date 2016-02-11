7938f0738f8cd5d1710f8b1331298022c310659e
Matt Reaves <mattmunee@hotmail.com>
2016-02-11 07:08:36 -0900

    add getTankChange

--- calculator.c
+++ calculator.c
@@ -1,10 +1,15 @@
 #include "calculator.h"
 
 OS_FLAG_GRP g_alarm_flags;
+OS_SEM g_sw1_sem;
 
 void post_alarms(CalculationState *currState){	  
   OSFlagPost(&g_alarm_flags, currState->current_alarm, OS_OPT_POST_FLAG_SET,&err);
   assert(OS_ERR_NONE == err);
+}
+
+uint16_t getTankChange(){
+  OSSemPost(&g_sw1_sem, OS_OPT_POST_1, &err);
 }
 
 void calculator_task(void* vptr) {
